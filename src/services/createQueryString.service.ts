7506ffca1f57a00d84c89ebda6a80d58cb5823d2
FermonMontego <fermonmantego@gmail.com>
2024-03-11 20:44:55 +0700

    important fix

--- src/services/createQueryString.service.ts
+++ src/services/createQueryString.service.ts
@@ -2,33 +2,36 @@
 
 const createQueryString = {
     create(params: string): URL {
-
-        const base_url: string = 'https://jsonplaceholder.typicode.com/users';
-        const tupleParams = params
-            .replace(/,\s*$/, '')
-            .split(',')
-            .map((param) => Number(param) ? Number(param) : param.trim());
+        const tupleParams = this.parseQuery(params);
 
         if (tupleParams.length === 1) {
-            let oneParams;
-
-            if (typeof tupleParams[0] === 'string' && tupleParams[0].split(' ').length < 2) {
-                oneParams = `?username=${tupleParams[0]}`;
-            } else if (typeof tupleParams[0] === 'string' && tupleParams[0].split(' ').length > 1) {
-                oneParams = `?name=${tupleParams[0]}`;
-            } else {
-                oneParams = `?id=${tupleParams[0]}`;
-            }
-
-            return new URL(`${base_url}${oneParams}`);
+            return this.createQueryStringOneParam(tupleParams);
         }
 
+        return this.createQueryMultiParams(tupleParams);
+    },
+
+    createQueryStringOneParam(tupleParams: (string | number)[]) {
+
+        let oneParams;
+
+        if (typeof tupleParams[0] === 'string' && tupleParams[0].split(' ').length < 2) {
+            oneParams = `?username=${tupleParams[0]}`;
+        } else if (typeof tupleParams[0] === 'string' && tupleParams[0].split(' ').length > 1) {
+            oneParams = `?name=${tupleParams[0]}`;
+        } else {
+            oneParams = `?id=${tupleParams[0]}`;
+        }
+
+        return this.createFullURL(String(oneParams));
+    },
+
+    createQueryMultiParams(tupleParams: (string | number)[]) {
         const queryString = tupleParams.reduce((acc, param, index) => {
             let typeofParam = typeof param;
 
             switch (typeofParam) {
                 case 'string':
-
                     if (typeof param === 'string' && param.split(' ').length > 1) {
                         return acc += index === 0 ? `?name=${param}` : `&name=${param}`;
                     } else {
@@ -40,9 +43,25 @@ const createQueryString = {
             }
 
             return acc;
-        }, base_url)
+        }, '')
 
-        return new URL(String(queryString));
+        return this.createFullURL(String(queryString));
+    },
+
+    parseQuery(query: string) {
+        const resultQuery = query
+            .replace(/,\s*$/, '')
+            .split(',')
+            .map((param) => Number(param) ? Number(param) : param.trim())
+            .filter(Boolean);
+
+        console.log(resultQuery);
+        return resultQuery;
+    },
+
+    createFullURL(stringWithParams: string) {
+        const base_url: string = 'https://jsonplaceholder.typicode.com/users';
+        return new URL(`${base_url}${stringWithParams}`);
     }
 }
 
